ac3fab906544392fc63072e2ad71ef99ab579008
astakhov <astakhov@iep.ru>
2025-01-01 19:50:31 +0300

    DZ

--- src/js/__tests__/Validator.test.js
+++ src/js/__tests__/Validator.test.js
@@ -7,21 +7,29 @@ describe('Validator', () => {
     validator = new Validator();
   });
 
-  test('valid username', () => {
-    expect(validator.validateUsername('valid_user123')).toBe(true);
-    expect(validator.validateUsername('user-name')).toBe(true);
-    expect(validator.validateUsername('user123')).toBe(true);
+  const validUsernames = [
+    'valid_user123',
+    'user-name',
+    'user123'
+  ];
+
+  const invalidUsernames = [
+    '123user',
+    'user-',
+    '-user',
+    '__user',
+    'user__',
+    'user--',
+    'user--name',
+    'user1234',
+    'user12345'
+  ];
+
+  test.each(validUsernames)('valid username: %s', (username) => {
+    expect(validator.validateUsername(username)).toBe(true);
   });
 
-  test('invalid usernames', () => {
-    expect(validator.validateUsername('123user')).toBe(false);
-    expect(validator.validateUsername('user-')).toBe(false);
-    expect(validator.validateUsername('-user')).toBe(false);
-    expect(validator.validateUsername('__user')).toBe(false);
-    expect(validator.validateUsername('user__')).toBe(false);
-    expect(validator.validateUsername('user--')).toBe(false);
-    expect(validator.validateUsername('user--name')).toBe(false);
-    expect(validator.validateUsername('user1234')).toBe(false);
-    expect(validator.validateUsername('user12345')).toBe(false);
+  test.each(invalidUsernames)('invalid username: %s', (username) => {
+    expect(validator.validateUsername(username)).toBe(false);
   });
 });
